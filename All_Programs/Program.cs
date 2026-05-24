a9384bddd4d89c58346d69cb86e8500e74f8ca8f
Ratul <168064170+ratul-03@users.noreply.github.com>
2026-05-24 18:20:28 +0600

    Second Commit

--- All_Programs/Program.cs
+++ All_Programs/Program.cs
@@ -6,6 +6,29 @@ namespace All_Programs
   {
     static void Main(string[] args)
     {
+      for (int i = 0; i < 5; i++)
+      {
+        // Console.WriteLine("I love C#");
+      }
+
+      /*
+      Console.Write("How many times do you want to say Hi: ");
+      string userInput = Console.ReadLine()!;
+      int counter = Convert.ToInt32(userInput);
+
+      if (counter <= 0)
+      {
+        Console.WriteLine("Please enter above 0");
+      }
+      else
+      {
+        for (int i = 1; i <= counter; i++)
+        {
+          Console.WriteLine("Hi");
+        }
+      }
+      */
+
       
     }
   }
